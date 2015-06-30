12d4b7768f88b1cb12e39d2eecc74fad03f1d209
Jeka Moroz <eugen.v.moroz@gmail.com>
2015-06-30 15:38:31 +0300

    translations

--- languages/en/tl_content.php
+++ languages/en/tl_content.php
@@ -12,7 +12,10 @@
  */
 
 $GLOBALS['TL_LANG']['tl_content']['aggregatorContent_legend'] = 'Konfiguration';
+$GLOBALS['TL_LANG']['tl_content']['posts_legend'] = 'Posts';
 $GLOBALS['TL_LANG']['tl_aggregator']['numPosts'][0] = 'Number of items';
 $GLOBALS['TL_LANG']['tl_aggregator']['numPosts'][1] = 'Here you can limit the number of items.';
 $GLOBALS['TL_LANG']['tl_aggregator']['channels'][0] = 'Sources';
 $GLOBALS['TL_LANG']['tl_aggregator']['channels'][1] = 'Define one or more sources to be displayed in the aggregator.';
+$GLOBALS['TL_LANG']['tl_aggregator']['refresh'] = 'Refresh';
+$GLOBALS['TL_LANG']['tl_aggregator']['posts'] = 'Posts';
